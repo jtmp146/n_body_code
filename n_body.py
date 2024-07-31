8a846a67b541bd8ff553e710c7af38fd1f3aa8d8
James Mackenzie Page <jtmp1@st-andrews.ac.uk>
2024-07-31 11:49:10 +0100

    First day's work implementing parallelism

--- n_body.py
+++ n_body.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 from matplotlib.animation import FuncAnimation
+from multiprocessing import Pool
 
 # np.random.seed(19680807)
 
@@ -12,7 +12,6 @@ mass_max = 1
 mass_min = 1
 G = 2.5
 length = 500
-fig_size = 15
 
 path = np.zeros(shape=(n, length, 3))
 pos = np.random.uniform(-start_range, start_range, size=(n, 3))
@@ -23,37 +22,41 @@ deleted = []
 def mag(v):
     return np.sqrt(v[0]**2+v[1]**2+v[2]**2)
 
+def remove(arr, i):
+    shape = (arr.shape[0]-1, arr.shape[1:])
+    arr2 = np.zeros(shape=shape)
+    arr2[:i] = arr[:i]
+    arr2[i:] = arr[i+1:]
+    return arr2
+
+def a(i, pos, mass):
+    a = 0
+    pos_i = pos[i]
+    pos_j = remove(pos, i)
+    mass_j = remove(pos, i)
+    r = pos_j - pos_i
+    r3 = np.sum(r**2, axis=1)**(3/2)
+    a = G*np.sum(r*(mass_j/r3)[:,np.newaxis], axis=0)
+    return a
+
+def update_i(args):
+    i, pos1, vel1, mass = args
+    vel = vel1+a(i, pos1, mass)*dt
+    pos = pos1+vel*dt
+    return i, pos, vel
+
 def update(num, pos=pos):
     ax.cla()
-    t = num/10
     dt = 0.1
+    t = num*dt
+    tasks = [(i, pos, vel, mass) for i in range]
     for i in range(n):   
-        a = 0
-        for j in range(n):
-            if j == i and j not in deleted:
-                ()
-            elif i not in deleted:
-                r = pos[j] - pos[i]
-                if mag(r) > mass[j]**(1/3)+mass[i]**(1/3):
-                    a += r*G*mass[j]/(mag(r)**3)
-                else:
-                    vel[i] = (mass[i]*vel[i]+mass[j]*vel[j])/(mass[i]+mass[j])
-                    mass[i] += mass[j]
-                    mass[j] = 0
-                    deleted.append(j)
-            else:
-                vel[i] = 0
-                    
-        vel[i] = vel[i]+a*dt
+        vel[i] = vel[i]+a(i)*dt
         pos[i] = pos[i]+vel[i]*dt
     for i in range(n):
         path[i][num] = pos[i]
         ax.scatter(pos[i][0], pos[i][1], pos[i][2], s=100*mass[i]**(1/3), marker="o")
         ax.plot([pos[0] for pos in path[i][0:num]], [pos[1] for pos in path[i][0:num]], [pos[2] for pos in path[i][0:num]])
-
-    # ax.set_xlim(-fig_size, fig_size)
-    # ax.set_ylim(-fig_size, fig_size)
-    # ax.set_zlim(-fig_size, fig_size)
 
 
 fig = plt.figure(dpi=100)
