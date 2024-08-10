9325559aa33dc3a44d0604e5f4522fc768d66edc
James Mackenzie Page <jtmp1@st-andrews.ac.uk>
2024-08-10 11:52:39 +0100

    Started implementing new merging method

--- n_body.py
+++ n_body.py
@@ -7,9 +7,9 @@ import os
 
 # np.random.seed(19680807)
 
-n = 25
+n = 2
 start_range = 15
-vel_range = 1
+vel_range = 0
 mass_max = 1
 mass_min = 1
 G = 2.5
@@ -18,6 +18,7 @@ length = 500
 pos = np.random.uniform(-start_range, start_range, size=(n, 3))
 vel = np.random.uniform(-vel_range, vel_range, size=(n, 3))
 mass = np.random.uniform(mass_min, mass_max, size=(n))
+mergers = set()
 
 def size(mass_i):
     return 200*mass_i**(1/3)
@@ -56,18 +57,36 @@ def a(i, pos, mass):
         a_i = 0
     return a_i
 
-def step_i(args):
-    i, pos, vel, mass, dt = args
-    # pos_j = np.delete(pos, i, 0)
-    # mass_j = np.delete(mass, i, 0)
-    # for j in len(pos_j):
-    #     if mag(pos[j]-pos[i]) < mass[j]**(1/3)+mass[i]**(1/3):
-    #         if mass[i] >= mass[j]:
+def merge_ij(i, j, mass):
+    if mass[i] > mass[j]:
+        mass_i = mass[i] + mass[j]
+        vel_i = (mass[i]*vel[i]+mass[j]*vel[j])/(mass_i)
+    elif mass[i] < mass[j]:
+        mass_i = 0.0
+        vel_i = 0.0
+    else:
+        if i > j:
+            mass_i = mass[i] + mass[j]
+            vel_i = (mass[i]*vel[i]+mass[j]*vel[j])/(mass_i)
+        else:
+            mass_i = 0.0
+            vel_i = 0.0
+    return mass_i, vel_i
 
-    vel_i = vel[i]+a(i, pos, mass)*dt
+def step_i(args):
+    i, pos, vel, mass, dt, mergers = args
+    for merger in mergers:
+        if i in merger:
+            mass_i, vel_i = merge_ij(merger[0], merger[1], mass)
+            break
+        else:
+            vel_i = vel[i]+a(i, pos, mass)*dt
+            mass_i = mass[i]
     pos_i = pos[i]+vel_i*dt
-    mass_i = mass[i]
-    result = (i, pos_i, vel_i, mass_i)
+    for j in range(pos.shape[0]):
+        r = mag(pos[j] - pos[i])
+        if 
+    result = (i, pos_i, vel_i, mass_i, new_mergers)
     return result
 
 if __name__ == '__main__':
@@ -81,12 +100,14 @@ if __name__ == '__main__':
         f = open("n_body.txt", "a")
 
         for frame in range(length):
-            tasks = [(i, pos, vel, mass, dt) for i in range(n)]
+            tasks = [(i, pos, vel, mass, dt, mergers) for i in range(n)]
             results = pool.map(step_i, tasks)
-            for i, pos_i, vel_i, mass_i in results:
+            for i, pos_i, vel_i, mass_i, new_mergers in results:
                 pos[i] = pos_i
                 vel[i] = vel_i
                 mass[i] = mass_i
+                for merger in new_mergers:
+                    mergers.add(merger)
             f.write(csv(pos, mass))
         f.close()
     stop = t.time()
